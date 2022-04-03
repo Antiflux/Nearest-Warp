23621d9f3239e5a33cbeb164f8f9ef0e6daf46c1
Antiflux <100823968+Antiflux@users.noreply.github.com>
2022-04-03 00:22:19 -0400

    Added newly-opened warps

--- main.js
+++ main.js
@@ -44,17 +44,22 @@ function run() {
 
 // alert('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
 
-// Standard warps
+// System warps
 warps.push(new Warp("Spawn", 110, -32));
 warps.push(new Warp("Elytra Boost", 153, -105));
+
+// Biome warps
 warps.push(new Warp("Roofed Forest", -3677, 4453));
 warps.push(new Warp("Jungle", -5763, 698));
 warps.push(new Warp("Savannah", 4182, -1776));
+warps.push(new Warp("Mega Taiga", -628, 2176));
 warps.push(new Warp("Plains", -3846, 7094));
+warps.push(new Warp("Mesa", 3091, -732));
 warps.push(new Warp("Desert", 2546, 1269));
 warps.push(new Warp("Mountain", 954, 2576));
 warps.push(new Warp("Cave", -2080, 3737));
 warps.push(new Warp("Island", 2324, -931));
+warps.push(new Warp("Flower Forest", 2353, 6959));
 warps.push(new Warp("Snowy Plains", -5628, 7571));
 
 // Event warps
