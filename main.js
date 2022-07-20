59176404004cbc31160e97ded5c9df3b76754b75
Antiflux <100823968+Antiflux@users.noreply.github.com>
2022-07-20 12:51:19 -0400

    Re-ordered the warps, added Swamp and Event Center

--- main.js
+++ main.js
@@ -45,11 +45,12 @@ function run() {
 // alert('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
 
 // System warps
+warps.push(new Warp("INVALID", 999999,999999,999999));
+warps.push(new Warp("Spawn", 110, -32));
+warps.push(new Warp("Build PVP", 8929, -1129));
 warps.push(new Warp("Comfy Fishing", 2622, 110));
 warps.push(new Warp("Elytra Boost", 153, -105));
 warps.push(new Warp("PVP Mountain", -7618, 7147));
-warps.push(new Warp("Spawn", 110, -32));
-warps.push(new Warp("Build PVP", 8929, -1129));
 
 // Biome warps
 warps.push(new Warp("Cave", -2080, 3737));
@@ -60,13 +61,14 @@ warps.push(new Warp("Jungle", -5763, 698));
 warps.push(new Warp("Mega Taiga", -628, 2176));
 warps.push(new Warp("Mesa", 3091, -732));
 warps.push(new Warp("Mountain", 954, 2576));
+warps.push(new Warp("Mushroom Village", 12187, 7231));
 warps.push(new Warp("Plains", -3846, 7094));
 warps.push(new Warp("Roofed Forest", -3677, 4453));
 warps.push(new Warp("Savannah", 4182, -1776));
 warps.push(new Warp("Snowy Plains", -5628, 7571));
+warps.push(new Warp("Swamp", -7342, -14615));
 warps.push(new Warp("\"Temp Warp\"", 3645, -6828));
-warps.push(new Warp("Mushroom Village", 12187, 7231));
 
 // Event warps
 // No event warps yet.
-// warps.push(new Warp("Event Center", 0, 0));
+warps.push(new Warp("Event Center", -3323, -251));
