dbb10181f79eb472c3060e16d184688c7ed58f53
Antiflux <100823968+Antiflux@users.noreply.github.com>
2023-09-10 23:58:16 -0400

    Added "Archaeology" location (Izonti research dig)

--- main.js
+++ main.js
@@ -106,6 +106,7 @@ warps.push(new Warp("Swamp", -7342, -14615, 60));
 warps.push(new Warp("\"Temp Warp\"", 3645, -6828, 70));
 
 // Event warps
+warps.push(new Warp("Archaeology (Jeep down the road, near Spawn)", 3164, -20026, 344));
 warps.push(new Warp("Event Center", -3323, -251, 71));
 warps.push(new Warp("March 2023 Build Comp (via Event Center, Build Competition)", -7300, 10000, 99));
 warps.push(new Warp("Obstacle Course Spectator (via Event Center, Weekly Events)", 7607, 9446, 110));
