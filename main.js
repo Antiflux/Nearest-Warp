c4d0663d6ca0915182c83665470ac8ef835ef0ea
Antiflux <100823968+Antiflux@users.noreply.github.com>
2025-01-24 01:23:23 -0500

    Removed all warps
    The world map that began in February of 2022 on the Safe Survival minecraft server has been closed. The server will reopen with a new map on Jan 26, 2025. All recorded warp locations have been wiped, and the coordinates of the new Spawn and warps will begin being added anew once the new world map is opened.

--- main.js
+++ main.js
@@ -78,66 +78,14 @@ function run() {
 	out.innerHTML = str;
 }
 
-// alert('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
-
 // System warps
 warps.push(new Warp("INVALID", 999999,999999,999999));
-warps.push(new Warp("Spawn", 110, -32));
-warps.push(new Warp("Build PVP", 8929, -1129, 76));
-warps.push(new Warp("Celestial Citadel PVP", 26960, 35720, 75));
-warps.push(new Warp("Comfy Fishing", 2622, 110, 71));
-warps.push(new Warp("Elytra Boost", 153, -105, 71));
-warps.push(new Warp("Mob Farm", -2077, 12495, 72));
-warps.push(new Warp("PVP Mountain", -7618, 7147, 78));
+// warps.push(new Warp("Spawn", 0, 0));
 
 // Biome warps
-warps.push(new Warp("Cave", -2080, 3737, 70));
-warps.push(new Warp("Cherry Grove", -22345, -20092, 62));
-warps.push(new Warp("Desert", 2546, 1269, 71));
-warps.push(new Warp("Flower Forest", 2353, 6959, 62));
-warps.push(new Warp("Island", 2324, -931, 66));
-warps.push(new Warp("Jungle", -5763, 698, 66));
-warps.push(new Warp("Mega Taiga", -628, 2176, 70));
-warps.push(new Warp("Mesa", 3091, -732, 71));
-warps.push(new Warp("Mountain", 954, 2576, 71));
-warps.push(new Warp("Mushroom Village", 12187, 7231, 64));
-warps.push(new Warp("Plains", -3846, 7094, 72));
-warps.push(new Warp("Roofed Forest", -3677, 4453, 64));
-warps.push(new Warp("Savannah", 4182, -1776, 68));
-warps.push(new Warp("Snowy Plains", -5628, 7571, 60));
-warps.push(new Warp("Swamp", -7342, -14615, 60));
-warps.push(new Warp("\"Temp Warp\"", 3645, -6828, 70));
 
 // Event warps
-warps.push(new Warp("Izonti Dig (Jeep down the road, near Spawn)", 3164, -20026, 344));
-warps.push(new Warp("Event Center", -3323, -251, 71));
-warps.push(new Warp("March 2023 Build Comp (via Event Center, Build Competition)", -7300, 10000, 99));
-warps.push(new Warp("Obstacle Course Spectator (via Event Center, Weekly Events)", 7607, 9446, 110));
-warps.push(new Warp("Team Axolotl (via Event Center, Build Competition)", -8447, 8595, 94));
-warps.push(new Warp("Team Shark (via Event Center, Build Competition)", -17003, 1474, 96));
-warps.push(new Warp("Team Snake (via Event Center, Build Competition)", 12176, -3923, 98));
-warps.push(new Warp("Treasure Hunt (via Event Center, Weekly Events)", 11098, -7629, 114));
-warps.push(new Warp("Spleef Spectator (via Event Center, Weekly Events)", -3610, 103, 127));
-warps.push(new Warp("Team 1: Dragon (via Event Center, Build Competition)", 17278, -21124, 100));
-warps.push(new Warp("Team 2: Caladrius (via Event Center, Build Competition)", 9014, 9072, 102));
-warps.push(new Warp("Team 3: Unicorn (via Event Center, Build Competition)", -17348, 564, 104));
 
 // Monthly Build Highlights (player builds)
-warps.push(new Warp("Safe Survival City (Event Center, Other Events, November 2023 Build Highlight)", -1264, 3295, 100));
-warps.push(new Warp("ItsImmi's Base (Event Center, Other Events, December 2023 Build Highlight)", -7939, 15461, 100));
-warps.push(new Warp("oBlazin's Base (Event Center, Other Events, January 2024 Build Highlight)", -15634, -3633, 101 + 25));
-// oBlazin's warp brings you in at the bottom and there are two bubble columns to the surface that are both approximately
-// twenty-five blocks from the arrival point. There's no easy simple way to account for that _properly_, so just add 25.
-warps.push(new Warp("IceHorror's Base (Event Center, Other Events, February 2024 Build Highlight)", -1596, 26952, 103));
-warps.push(new Warp("Hyth4n's Tree (Event Center, Other Events, March 2024 Build Highlight)", -16516, 3715, 105));
-warps.push(new Warp("KStru627's Keep (Event Center, Other Events, April 2024 Build Highlight)", -20680, -30, 105));
-// Louguts' Knowledge Center is _directly at_ Mega Taiga Warp
-// warps.push(new Warp("louguts' Knowledge Center (Event Center, Other Events, May 2024 Build Highlight)", -675, 2167, 104));
-// Astley Garden is _directly at_ Roofed Forest Warp
-// warps.push(new Warp("Astley Garden by MusicOwner (Event Center, Other Events, June 2024 Build Highlight)", -3630, 4454, 104));
-warps.push(new Warp("Istrella by benjib0o (Event Center, Other Events, July 2024 Build Highlight)", -507, 536, 101));
-warps.push(new Warp("1way's Island (Event Center, Other Events, August 2024 Build Highlight)", -16092, 20465, 100));
-
-warps.push(new Warp("berry_boba's Grove (September 2024 Build Highlight)", 17405, 22329, 6));
 
 // End
